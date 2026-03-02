0c94ee31e28bd56bbfc144254d302da1cf375033
Natthawut Putthong <68010375@kmitl.ac.th>
2026-03-02 10:27:35 +0700

    l

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller()
-export class UserController  {
-  constructor(private readonly userService:UserService ) {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
 
   @Get('test')
-  test(): string[]{
+  test(): string[] {
     return this.userService.test();
   }
 }
