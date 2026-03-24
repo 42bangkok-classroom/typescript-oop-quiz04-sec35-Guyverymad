fa1484da91ce58b68617900103c354831f126e17
Natthawut Putthong <68010375@kmitl.ac.th>
2026-03-24 13:41:48 +0700

    vs

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { IUser } from './user.interface';
 @Controller('users')
