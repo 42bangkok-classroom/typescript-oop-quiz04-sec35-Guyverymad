a88eadeeca0a3219f2fb1b03672609b5a98f4bb3
Natthawut Putthong <68010375@kmitl.ac.th>
2026-03-24 22:58:12 +0700

    an

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { IUser } from './user.interface';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('test')
-  test() {
+  test(): IUser[] {
     return this.userService.test();
+  }
+  @Get()
+  findAll(): IUser[] {
+    return this.userService.findAll();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { IUser } from './user.interface';
+import * as fs from 'fs';
+import * as path from 'path';
 @Injectable()
 export class UserService {
   test(): IUser[] {
     return [];
+  }
+  findAll(): IUser[] {
+    const filePath = path.join(process.cwd(), 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath, 'utf-8');
+    const users: IUser[] = JSON.parse(fileData) as unknown as IUser[];
+    return users;
   }
 }
