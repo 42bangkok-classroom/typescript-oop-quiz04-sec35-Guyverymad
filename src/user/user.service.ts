c5a82dfc3805901e78369b827dc4ff5aad2719a2
Natthawut Putthong <68010375@kmitl.ac.th>
2026-03-24 23:17:21 +0700

    ao

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,10 +7,11 @@ export class UserService {
   test(): IUser[] {
     return [];
   }
-  findAll(): IUser[] {
+   findAll(): IUser[] {
     const filePath = path.join(process.cwd(), 'data', 'users.json');
     const fileData = fs.readFileSync(filePath, 'utf-8');
     const users: IUser[] = JSON.parse(fileData) as unknown as IUser[];
     return users;
   }
+  
 }
